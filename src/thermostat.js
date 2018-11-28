0da6363328aa1f477224e51f93c15316f206bee2
Will Wayman <will.jdw@gmail.com>
2018-11-28 14:09:12 +0000

    adapted functions to prototype

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,49 +1,45 @@
 function Thermostat () {
-  Thermostat.temp = 20;
-  Thermostat.isPowerSaving = true;
+  this.temp = 20;
+  this.isPowerSaving = true;
+};
 
-
-  Thermostat.up = function() {
-    if ( this.isPowerSaving ) {
-      if ( this.temp < 25 ) {
-        this.temp ++;
-      };
-    }
-    else {
-      if ( this.temp < 32 ) {
-        this.temp ++;
-      };
+Thermostat.prototype.up = function() {
+  if ( this.isPowerSaving ) {
+    if ( this.temp < 25 ) {
+      this.temp ++;
+    };
+  }
+  else {
+    if ( this.temp < 32 ) {
+      this.temp ++;
     };
   };
+};
 
-  Thermostat.down = function() {
-      if ( this.temp > 10 ) {
-        this.temp --;
-      // }
-      // else {
-      //   alert( "Minimum temperature is 10 degree!")
-    };
+Thermostat.prototype.down = function() {
+    if ( this.temp > 10 ) {
+      this.temp --;
+    // }
+    // else {
+    //   alert( "Minimum temperature is 10 degree!")
   };
+};
 
-  Thermostat.energyUsage = function() {
-    var text = "high-usage"
-    if (this.temp < 18) {
-      text =  "low-usage";
-    } else if (this.temp < 25) {
-      text = "medium-usage";
-    };
-    return text
+Thermostat.prototype.energyUsage = function() {
+  var text = "high-usage"
+  if (this.temp < 18) {
+    text =  "low-usage";
+  } else if (this.temp < 25) {
+    text = "medium-usage";
   };
+  return text
+};
 
 
-  Thermostat.togglePowerSaving = function() {
-    this.isPowerSaving = !this.isPowerSaving;
-  };
+Thermostat.prototype.togglePowerSaving = function() {
+  this.isPowerSaving = !this.isPowerSaving;
+};
 
-  Thermostat.reset = function() {
-    this.temp = 20;
-  };
-
-
-
+Thermostat.prototype.reset = function() {
+  this.temp = 20;
 };
